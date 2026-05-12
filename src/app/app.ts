e1d45a577d2e77bf5e12c9d2aa226dafdb1afcea
abdelrahman <abdelrahman.kamel.dev@gmail.com>
2026-05-12 15:35:21 +0300

    main app style

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,14 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Navbar } from './components/navbar/navbar';
+import { Post } from './components/post/post';
+import { Footer } from './components/footer/footer';
+import { CreatePost } from './components/create-post/create-post';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Navbar, Post, Footer, CreatePost],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('Streamline');
